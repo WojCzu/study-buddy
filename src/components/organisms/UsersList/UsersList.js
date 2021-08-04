e993d28332393e6db540a499e35a8673f2edda71
WojCzu <wojciech.czuma@gmail.com>
2021-08-04 13:12:14 +0200

    simple routing

--- src/components/organisms/UsersList/UsersList.js
+++ src/components/organisms/UsersList/UsersList.js
@@ -1,81 +1,20 @@
-import React, { useState } from 'react';
-import { users as usersData } from 'data/users';
+import React from 'react';
 import UsersListItem from 'components/molecules/UsersListItem/UsersListItem';
 import { Wrapper, StyledList, StyledTitle } from './UsersList.styles';
-import FormField from 'components/molecules/FormField/FormField';
-import { Button } from 'components/atoms/Button/Button';
 
-const initialFormState = {
-  name: '',
-  attendance: '',
-  average: '',
-};
-
-const UsersList = () => {
-  const [users, setUsers] = useState(usersData);
-  const [formValues, setFormValues] = useState(initialFormState);
-
-  const deleteUser = (name) => {
-    const filteredUsers = users.filter((user) => user.name !== name);
-    setUsers(filteredUsers);
-  };
-
-  const handleInputChange = (e) => {
-    setFormValues({ ...formValues, [e.target.name]: e.target.value });
-  };
-
-  const handleAddUser = (e) => {
-    e.preventDefault();
-    const newUser = {
-      name: formValues.name,
-      attendance: formValues.attendance,
-      average: formValues.average,
-    };
-    setUsers([newUser, ...users]);
-    setFormValues(initialFormState);
-  };
-
-  return (
-    <>
-      <Wrapper as="form" onSubmit={handleAddUser}>
-        <StyledTitle>Add new student</StyledTitle>
-        <FormField
-          label="Name"
-          id="name"
-          name="name"
-          value={formValues.name}
-          onChange={handleInputChange}
+const UsersList = ({ users, deleteUser }) => (
+  <Wrapper>
+    <StyledTitle>Students list</StyledTitle>
+    <StyledList>
+      {users.map((userData) => (
+        <UsersListItem
+          key={userData.name}
+          userData={userData}
+          deleteUser={deleteUser}
         />
-        <FormField
-          label="Attendance"
-          id="attendance"
-          name="attendance"
-          value={formValues.attendance}
-          onChange={handleInputChange}
-        />
-        <FormField
-          label="Average"
-          id="average"
-          name="average"
-          value={formValues.average}
-          onChange={handleInputChange}
-        />
-        <Button type="submit">Add</Button>
-      </Wrapper>
-      <Wrapper>
-        <StyledTitle>Students list</StyledTitle>
-        <StyledList>
-          {users.map((userData) => (
-            <UsersListItem
-              key={userData.name}
-              userData={userData}
-              deleteUser={deleteUser}
-            />
-          ))}
-        </StyledList>
-      </Wrapper>
-    </>
-  );
-};
+      ))}
+    </StyledList>
+  </Wrapper>
+);
 
 export default UsersList;
