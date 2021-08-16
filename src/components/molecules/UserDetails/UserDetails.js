a638ac7e1780b75c5a5bbb844e5ac061fc98b151
WojCzu <wojciech.czuma@gmail.com>
2021-08-16 17:11:32 +0200

    add styles to user details

--- src/components/molecules/UserDetails/UserDetails.js
+++ src/components/molecules/UserDetails/UserDetails.js
@@ -1,16 +1,51 @@
 import React from 'react';
 import { Title } from 'components/atoms/Title/Title';
 import { StyledAverage } from 'components/molecules/UsersListItem/UsersListItem.styles';
-
+import DeleteButton from 'components/atoms/DeleteButton/DeleteButton';
+import {
+  Wrapper,
+  HeaderWrapper,
+  ContentWrapper,
+  Detail,
+  Grades,
+} from './UserDetails.styles';
 const UserDetails = ({ student }) => {
   return (
-    <div>
-      <Title>
-        {student.name} | Group {student.group}
-      </Title>
-      <p>{student.attendance}</p>
-      <StyledAverage value={student.average}>{student.average}</StyledAverage>
-    </div>
+    <Wrapper>
+      <HeaderWrapper>
+        <StyledAverage value={student.average}>{student.average}</StyledAverage>
+        <Title>{student.name}</Title>
+        <DeleteButton />
+      </HeaderWrapper>
+      <ContentWrapper>
+        <Detail>
+          <p>Course:</p>
+          <p>Economy and finances</p>
+        </Detail>
+        <Detail>
+          <p>
+            Attendance: <span>{student.attendance}</span>
+          </p>
+        </Detail>
+        <Detail>
+          <p>Average grades:</p>
+          <Grades>
+            <div>
+              <p>Modern Economy</p>
+              <StyledAverage value={2.8}>2.8</StyledAverage>
+            </div>
+            <div>
+              <p>Trade and logistics</p>
+              <StyledAverage value={3.8}>3.8</StyledAverage>
+            </div>
+            <div>
+              <p>Business Philosophy</p>
+              <StyledAverage value={5.0}>5.0</StyledAverage>
+            </div>
+          </Grades>
+        </Detail>
+      </ContentWrapper>
+    </Wrapper>
   );
 };
 
---
+++ src/components/molecules/UserDetails/UserDetails.styles.js
@@ -0,0 +1,65 @@
+import { Title } from 'components/atoms/Title/Title';
+import { StyledAverage } from 'components/molecules/UsersListItem/UsersListItem.styles';
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  width: 100%;
+`;
+export const HeaderWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  margin-bottom: 50px;
+
+  ${StyledAverage} {
+    width: 68px;
+    height: 68px;
+
+    font-size: ${({ theme }) => theme.fontSize.xl};
+  }
+  ${Title} {
+    margin: 0;
+  }
+`;
+export const ContentWrapper = styled.div`
+  margin-left: 34px;
+  font-size: ${({ theme }) => theme.fontSize.l};
+  color: ${({ theme }) => theme.colors.darkGrey};
+`;
+
+export const Detail = styled.div`
+  margin: 20px 0;
+
+  p {
+    margin: 0;
+  }
+  p:nth-child(2) {
+    font-size: ${({ theme }) => theme.fontSize.xl};
+  }
+
+  > :first-child {
+    font-weight: bold;
+    span {
+      font-weight: normal;
+    }
+  }
+  > :not(:first-child) {
+    letter-spacing: 0.03em;
+  }
+`;
+export const Grades = styled.div`
+  width: fit-content;
+  & > div {
+    display: flex;
+    justify-content: space-between;
+    margin: 15px 0;
+
+    ${StyledAverage} {
+      width: 40px;
+      height: 40px;
+      margin-left: 40px;
+      font-size: ${({ theme }) => theme.fontSize.m};
+    }
+  }
+`;
